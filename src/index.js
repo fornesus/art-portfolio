bf48ffe3327bfc2e36eb085a017288f1a285660f
Chris Fornesa <17108039+cftxp@users.noreply.github.com>
2019-09-11 20:30:22 -0500

    serviceWorker Import 2
    Second attempt to import the serviceWorker.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
 
 // App components:
 import App from './App';
