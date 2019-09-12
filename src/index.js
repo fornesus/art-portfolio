61a57f6ef9c30dcd01a8dec70fde19db1b4e3312
Chris Fornesa <17108039+cftxp@users.noreply.github.com>
2019-09-11 20:46:38 -0500

    Manual reversion
    Manual reversion back to version a047f79f47202bd6ccb73e2cc01717b4dc7e931f.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 // App components:
 import App from './App';
