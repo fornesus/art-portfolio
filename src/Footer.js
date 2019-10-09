93149da3bd73742df132e3528655ff7473d54ffb
Christopher Fornesa <admin@cftxp.net>
2019-10-08 19:03:31 -0500

    Update Footer
    Update the footer links to include the new Django version of this application.

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     let date = new Date();
     let year = date.getFullYear();
 
-    return <p><a href="https://www.chrisfornesa.com" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || <a href="https://art.php.chrisfornesa.com">This site in PHP</a></p>;
+    return <p><a href="https://www.chrisfornesa.com" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || This site in: <a href="https://art.php.chrisfornesa.com">PHP</a> | <a href="https://art.django.chrisfornesa.com">Django</a></p>;
   }
 }
 
