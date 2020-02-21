53525b25458511277a1d6bd1df55b5e06b93dc52
Christopher Fornesa <admin@cftxp.net>
2020-02-20 21:17:11 -0600

    Reversal of Accidental Commit
    The last commit was accidental, this is a replacement.

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import './Footer.css';
 
 class Footer extends React.Component {
   render(){
-    return (
-      <div>
-        <p>
-          <a href="https://csf.li" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; 2020.
-        </p>
-      </div>
-    )
+    let date = new Date();
+    let year = date.getFullYear();
+
+    return <p><a href="https://www.fornesa.design" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || This site in: <a href="https://art.php.fornesa.design">PHP</a> | <a href="https://art.django.fornesa.design">Django</a></p>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
+import './index.css';
+// import * as serviceWorker from './serviceWorker';
 
-import Header from './Header';
+// App components:
 import App from './App';
+import List from './List';
 import Footer from './Footer';
 
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Header />, document.querySelector('header'));
-ReactDOM.render(<App />, document.querySelector('main'));
-ReactDOM.render(<Footer />, document.querySelector('footer'));
+// The main application component:
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<List />, document.querySelector('#content'));
+ReactDOM.render(<Footer />, document.querySelector("footer"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.register();
